43ac4d6899f9c4d5214636a3e30528706669863c
Juan Ruiz <juanruiz7201@gmail.com>
2022-04-12 12:14:41 -0700

    built login page layout

--- synth/pages/index.tsx
+++ synth/pages/index.tsx
@@ -1,10 +1,26 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
+import SynthWhiteLogo from "../assets/synth-white-logo.svg";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gradient-to-r from-black to-gray-700">
+      <div className="w-9/12 bg-white rounded h-3/6 sm:w-6/12 md:w-6/12 lg:w-3/12 ">
+        <div className="flex flex-col items-center justify-center h-3/6">
+          <Image
+            src={SynthWhiteLogo}
+            alt="synthesis white logo"
+            className="h-1/2"
+          />
+          <h1 className="flex items-end justify-center w-1/2 text-xl font-bold h-1/3">
+            Login
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
