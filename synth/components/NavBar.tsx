37f9457bcffe73dd531de8bb6001cabe28dcfb06
Juan Ruiz <juanruiz7201@gmail.com>
2022-04-19 16:22:17 -0700

    completed nav bar component

---
+++ synth/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+import SynthLogo from "../assets/synth-black-logo.svg";
+import downArrow from "../assets/down-arrow.svg";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-center w-full bg-black">
+      <div className="flex flex-row justify-between w-5/6">
+        <div className="flex flex-row">
+          <Image src={SynthLogo} alt="synthesis white logo" />
+          <p className="flex items-center px-8 text-sm text-white">
+            Availability
+          </p>
+        </div>
+
+        <div className="flex flex-row items-center justify-between w-1/6">
+          <p className="text-sm text-white">Juan</p>
+          <Image src={downArrow} alt="down arrow" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
