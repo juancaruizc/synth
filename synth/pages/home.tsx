96cf0c9dcbb779975534971ad4998787c537eb2a
Juan Ruiz <juanruiz7201@gmail.com>
2022-04-19 16:45:23 -0700

    added home-title

--- synth/pages/home.tsx
+++ synth/pages/home.tsx
@@ -6,6 +6,15 @@ const home = () => {
   return (
     <div>
       <NavBar />
+
+      {/* main home container */}
+      <div className="flex justify-center">
+        {/* title div */}
+        <div className="flex flex-row justify-between w-9/12">
+          <h1 className="flex items-center text-2xl text-white">Home</h1>
+          <button className="w-32 bg-white rounded-md h-11">New Cohort</button>
+        </div>
+      </div>
     </div>
   );
 };
