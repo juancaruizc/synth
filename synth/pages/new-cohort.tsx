21490a1f4fd2246bbdc92e90d0615aa891097a64
Juan Ruiz <juanruiz7201@gmail.com>
2022-04-22 11:12:36 -0700

    adjusted mobile styling in new cohort view

--- synth/pages/new-cohort.tsx
+++ synth/pages/new-cohort.tsx
@@ -8,11 +8,11 @@ import FacImage from "../assets/fac-image.svg";
 
 const NewCohort = () => {
   return (
-    <div>
+    <div className="h-fit ">
       <NavBar />
 
       {/* main section */}
-      <section className="flex flex-col items-center justify-center h-screen border-2 border-solid border-sky-500">
+      <section className="flex flex-col items-center justify-center h-fit">
         {/* select cohort container */}
         <div className="w-3/4 pt-16 ">
           <h1 className="pb-4 text-xl text-white">Select Your New Cohort</h1>
@@ -43,10 +43,10 @@ const NewCohort = () => {
         </div>
 
         {/* available cohorts container */}
-        <div className="w-3/4 h-screen pt-20">
-          <div className="flex flex-col items-center rounded-md h-96 bg-slate-700">
+        <section className="w-3/4 pt-20 h-fit">
+          <div className="flex flex-col items-center mb-12 rounded-md h-96 bg-slate-700">
             {/* top section */}
-            <div className="flex flex-col justify-center w-4/5 border-b-2 border-slate-10 h-1/3">
+            <div className="flex flex-col justify-center w-4/5 border-b-2 border-slate-10 h-1/2">
               <h2 className="pb-2 text-3xl text-white">Fish</h2>
               <h3 className="pb-2 text-base text-slate-300 ">lorem impsum</h3>
 
@@ -57,9 +57,9 @@ const NewCohort = () => {
             </div>
 
             {/* bottom section */}
-            <div className="flex flex-col items-center justify-center h-1/2">
+            <div className="flex flex-col items-center pt-4 h-3/4">
               {/* details container */}
-              <div className="flex flex-wrap justify-between w-4/5 pt-6 h-4/5">
+              <div className="flex flex-wrap justify-between w-4/5 pt-2 h-1/2">
                 {/* students */}
                 <div className="flex flex-col justify-center w-1/3">
                   <h3 className="text-sm text-white ">Students</h3>
@@ -91,23 +91,71 @@ const NewCohort = () => {
                 </div>
               </div>
 
-              {/* facilitator container */}
-              <div className="w-4/5">
-                <div className="flex items-center justify-center w-3/4 h-12 border border-slate-400">
-                  <div>
-                    <Image src={FacImage} alt="facilitator image" />
-                  </div>
-
-                  {/* facilitator details container */}
-                  <div className="w-40 h-12">
-                    <p className="w-3/4 text-white">Chrisman</p>
-                    <p className="w-3/4 text-slate-300">Facilitator</p>
-                  </div>
-                </div>
+              {/* sign up button container */}
+              <div className="w-4/5 pt-8 pb-2">
+                <button className="px-6 text-black transition-colors duration-300 bg-white rounded-lg shadow-lg py-2text-sm h-11 hover:bg-slate-300 shadow-orange-400 ">
+                  Sign Up
+                </button>
               </div>
             </div>
           </div>
-        </div>
+
+          <div className="flex flex-col items-center rounded-md h-96 bg-slate-700">
+            {/* top section */}
+            <div className="flex flex-col justify-center w-4/5 border-b-2 border-slate-10 h-1/2">
+              <h2 className="pb-2 text-3xl text-white">Fish</h2>
+              <h3 className="pb-2 text-base text-slate-300 ">lorem impsum</h3>
+
+              {/* cohort match container */}
+              <div className="flex items-center justify-center w-2/4 h-6 bg-green-600 rounded">
+                <p className="text-xs text-white">Cohort Match</p>
+              </div>
+            </div>
+
+            {/* bottom section */}
+            <div className="flex flex-col items-center pt-4 h-3/4">
+              {/* details container */}
+              <div className="flex flex-wrap justify-between w-4/5 pt-2 h-1/2">
+                {/* students */}
+                <div className="flex flex-col justify-center w-1/3">
+                  <h3 className="text-sm text-white ">Students</h3>
+                  <p className="text-sm text-slate-300">12</p>
+                </div>
+
+                {/* ages */}
+                <div className="flex flex-col justify-center">
+                  <h3 className="text-sm text-white">Ages</h3>
+                  <p className="text-sm text-slate-300">14-15</p>
+                </div>
+
+                {/* cohort */}
+                <div className="flex flex-col justify-center">
+                  <h3 className="text-sm text-white">Cohort</h3>
+                  <p className="text-sm text-slate-300">4324</p>
+                </div>
+
+                {/* start date */}
+                <div className="flex flex-col justify-center w-1/2">
+                  <h3 className="text-sm text-white">Start Date</h3>
+                  <p className="text-sm text-slate-300">May 12, 2022</p>
+                </div>
+
+                {/* meeting time */}
+                <div className="flex flex-col justify-center">
+                  <h3 className="text-sm text-white">Meeting Time</h3>
+                  <p className="text-sm text-slate-300">1:30 PM</p>
+                </div>
+              </div>
+
+              {/* sign up button container */}
+              <div className="w-4/5 pt-8 pb-2">
+                <button className="px-6 text-black transition-colors duration-300 bg-white rounded-lg shadow-lg py-2text-sm h-11 hover:bg-slate-300 shadow-orange-400 ">
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
       </section>
     </div>
   );
